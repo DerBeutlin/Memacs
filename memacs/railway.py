5cc2e7ee7cf813e28dcfeba15cf8f0c7f067495c
Max Beutelspacher <max.beutelspacher@mailbox.org>
2018-09-15 17:30:54 +0200

    add railway module
    add tests

---
+++ memacs/railway.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: <2012-03-09 15:48:38 armin>
+
+import logging
+import time
+import PyPDF2
+import os
+import re
+import datetime as dt
+from collections import namedtuple
+from .lib.orgproperty import OrgProperties
+from .lib.orgformat import OrgFormat
+from .lib.memacs import Memacs
+
+RailwayTicket = namedtuple('RailwayTicket', ['stops', 'stop_times'])
+class DBTicketParser:
+    def __init__(self):
+        pass
+
+    def extract_text(self, path):
+        with open(path, 'rb') as f:
+            pdfReader = PyPDF2.PdfFileReader(f)
+            page = pdfReader.getPage(0)
+            text = page.extractText()
+            return text
+
+    def find_in_text(self, regex, text):
+        compiled_regex = re.compile(regex)
+        result = compiled_regex.search(text)
+        if result is not None:
+            return result.group(1).strip()
+
+    def extract_date(self, text):
+        regex = re.compile('(Hinfahrt|ckfahrt) am (\d{2}.\d{2}.\d{4})')
+        result = regex.search(text)
+        if result is not None:
+            return dt.datetime.strptime(result.group(2).strip(),
+                                        '%d.%m.%Y').date()
+
+    def extract_stops(self, text, departure_date):
+        days = [departure_date + dt.timedelta(days=i) for i in range(10)]
+        compiled_regex = re.compile(
+            '([^\d\n]*)\n?(' + '|'.join([d.strftime('%d.%m.')
+                                         for d in days]) + ')(ab|an)')
+        results = compiled_regex.findall(text)
+        return [stop[0] for stop in results]
+
+    def extract_stop_times(self, text, stops, departure_date):
+        stop_times = []
+        for stop in set(stops):
+            compiled_regex = re.compile(
+                re.escape(stop) + '\n?(\d{2}.\d{2}).(ab|an)\s(\d{2}:\d{2})')
+            results = compiled_regex.findall(text)
+            for r in results:
+                day, month = r[0].split('.')
+                hour, minute = r[2].split(':')
+                if int(month) == 1 and departure_date.month == 12:
+                    year = departure_date.year + 1
+                else:
+                    year = departure_date.year
+
+                time = dt.datetime(
+                    year=year,
+                    month=int(month),
+                    day=int(day),
+                    hour=int(hour),
+                    minute=int(minute))
+                stop_times.append(time)
+        return sorted(stop_times)
+
+    def parse_ticket(self, text):
+        departure_date = self.extract_date(text)
+        stops = self.extract_stops(text, departure_date)
+        stop_times = self.extract_stop_times(text, stops, departure_date)
+        return RailwayTicket(stops=stops, stop_times=stop_times)
+
+    def parse_tickets(self, path):
+        text = self.extract_text(path)
+        index = text.find('ckfahrt am')
+        if index != -1:
+            ticket_texts = [text[:index], text[index:]]
+        else:
+            ticket_texts = [text]
+
+        return [self.parse_ticket(text) for text in ticket_texts]
+
+class Railway(Memacs):
+    available_parsers={'DB':DBTicketParser}
+    def _parser_add_arguments(self):
+        """
+        overwritten method of class Memacs
+
+        add additional arguments
+        """
+        Memacs._parser_add_arguments(self)
+
+        self._parser.add_argument(
+          "-f", "--folder", dest="ticket_folder",
+          action="append",
+          help="path to a folder to search for tickets" +\
+                                       "multiple folders can be specified: " + \
+                                       "-f /path1 -f /path2")
+        self._parser.add_argument("-C","--Company", dest="company",help="Railway Company [DB]")
+
+    def _parser_parse_args(self):
+        """
+        overwritten method of class Memacs
+
+        all additional arguments are parsed in here
+        """
+        Memacs._parser_parse_args(self)
+
+        if self._args.ticket_folder:
+            for f in self._args.ticket_folder:
+                if not os.path.isdir(f):
+                    self._parser.error("Check the folderlist argument: " +\
+                                       "[" + str(f) + "] and probably more aren't folders")
+        else:
+            self._parser.error("You have to provide the path to a directory")
+
+        if self._args.company:
+            if self._args.company not in self.available_parsers:
+                self._parser.error("There is no parser yet defined for {}".format(self._args.company))
+            else:
+                self.parser= self.available_parsers[self._args.company]()
+
+        else:
+            self.parser.error("You have to provide the company")
+    def __handle_file(self,path):
+        tickets = self.parser.parse_tickets(path)
+        for ticket in tickets:
+            timestamp = OrgFormat.datetime(ticket.stop_times[0])
+            end_timestamp = OrgFormat.datetime(ticket.stop_times[-1])
+            output =OrgFormat.link(link=path,description="Train ride from {} to {}".format(ticket.stops[0],ticket.stops[-1]),replacespaces=False)
+            properties = OrgProperties(data_for_hashing=output + timestamp)
+            properties.add('ORIGIN',ticket.stops[0])
+            properties.add('DESTINATION',ticket.stops[-1])
+            properties.add('DEPARTURE',OrgFormat.datetime(ticket.stop_times[1]))
+            properties.add('ARRIVAL',OrgFormat.datetime(ticket.stop_times[-1]))
+            self._writer.write_org_subitem(output=output,timestamp=timestamp+'-'+end_timestamp,properties=properties)
+
+
+
+    def _main(self):
+        """
+        get's automatically called from Memacs class
+        """
+
+        for folder in self._args.ticket_folder:
+            for dirpath,_,filenames in os.walk(folder):
+                for f in filenames:
+                    if f.endswith('.pdf'):
+                        abs_path = os.path.join(dirpath,f)
+                        self.__handle_file(abs_path)
---
+++ memacs/tests/railway_test.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+# Time-stamp: <2018-08-25 14:16:04 vk>
+
+import unittest
+import os
+import datetime as dt
+from memacs.railway import Railway, DBTicketParser
+
+
+class TestFoo(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_all(self):
+
+        path = os.path.dirname(os.path.abspath(__file__))
+        path = os.path.join(path,'data/dbtickets')
+        argv = "-f {} -C DB".format(path)
+        memacs = Railway(argv=argv.split())
+        data = memacs.test_get_entries()
+
+        
+        self.assertEqual(
+            data[0],
+            "** <2018-09-17 Mon 18:00>-<2018-09-17 Mon 19:21> [[{}/HinRueckfahrt.pdf][Train ride from Mannheim Hbf to Schwaikheim]]".format(path))
+        self.assertEqual(
+            data[1],
+            "   :PROPERTIES:")
+        self.assertEqual(
+            data[2],
+            "   :ORIGIN:      Mannheim Hbf")
+        self.assertEqual(
+            data[3],
+            "   :DESTINATION: Schwaikheim")
+        self.assertEqual(
+            data[4],
+            "   :DEPARTURE:   <2018-09-17 Mon 18:46>")
+        self.assertEqual(
+            data[5],
+            "   :ARRIVAL:     <2018-09-17 Mon 19:21>")
+        self.assertEqual(
+            data[6],
+            "   :ID:          87753377b46ce152b002fd9c2ceccab5a936c827")
+        self.assertEqual(
+            data[7],
+            "   :END:")
+        self.assertEqual(
+            data[8],
+            "** <2018-09-19 Wed 20:53>-<2018-09-19 Wed 22:29> [[{}/HinRueckfahrt.pdf][Train ride from Schwaikheim to Mannheim Hbf]]".format(path))
+        self.assertEqual(
+            data[9],
+            "   :PROPERTIES:")
+        self.assertEqual(
+            data[10],
+            "   :ORIGIN:      Schwaikheim")
+        self.assertEqual(
+            data[11],
+            "   :DESTINATION: Mannheim Hbf")
+        self.assertEqual(
+            data[12],
+            "   :DEPARTURE:   <2018-09-19 Wed 21:15>")
+        self.assertEqual(
+            data[13],
+            "   :ARRIVAL:     <2018-09-19 Wed 22:29>")
+        self.assertEqual(
+            data[14],
+            "   :ID:          4e89a5079544e50cf94116ee79b315ddc46da277")
+        self.assertEqual(
+            data[15],
+            "   :END:")
+        self.assertEqual(
+            data[16],
+            "** <2018-06-24 Sun 15:53>-<2018-06-24 Sun 17:18> [[{}/Hinfahrt.pdf][Train ride from Schwaikheim to Heidelberg Hbf]]".format(path))
+        self.assertEqual(
+            data[17],
+            "   :PROPERTIES:")
+        self.assertEqual(
+            data[18],
+            "   :ORIGIN:      Schwaikheim")
+        self.assertEqual(
+            data[19],
+            "   :DESTINATION: Heidelberg Hbf")
+        self.assertEqual(
+            data[20],
+            "   :DEPARTURE:   <2018-06-24 Sun 16:15>")
+        self.assertEqual(
+            data[21],
+            "   :ARRIVAL:     <2018-06-24 Sun 17:18>")
+        self.assertEqual(
+            data[22],
+            "   :ID:          d6e94fe3ccc0174073b69105c7516df9658631a3")
+        self.assertEqual(
+            data[23],
+            "   :END:")
+
+
+
+    def tearDown(self):
+        pass
+
+
+
+class TestDBTicketParser:
+
+    class TestOnlyOneDirectionTicket(unittest.TestCase):
+        def setUp(self):
+            parser = DBTicketParser()
+            path = os.path.dirname(os.path.abspath(__file__))
+            path = os.path.join(path,'data/dbtickets','Hinfahrt.pdf')
+            self.ticket = parser.parse_tickets(path)
+
+        def test_there_are_the_same_number_of_stops_as_stop_times(self):
+            assert len(self.ticket[0].stops) == len(self.ticket[0].stop_times)
+
+        def test_stops(self):
+            assert self.ticket[0].stops == ['Schwaikheim','Stuttgart Hbf (tief)','Stuttgart Hbf','Heidelberg Hbf']
+
+        def test_stop_times(self):
+            assert self.ticket[0].stop_times[0] == dt.datetime(year=2018,month=6,day=24,hour=15,minute=53)
+            assert self.ticket[0].stop_times[1] == dt.datetime(year=2018,month=6,day=24,hour=16,minute=15)
+            assert self.ticket[0].stop_times[2] == dt.datetime(year=2018,month=6,day=24,hour=16,minute=36)
+            assert self.ticket[0].stop_times[3] == dt.datetime(year=2018,month=6,day=24,hour=17,minute=18)
+
+    class TestBothDirectionTicket(unittest.TestCase):
+        def setUp(self):
+            parser = DBTicketParser()
+            path = os.path.dirname(os.path.abspath(__file__))
+            path = os.path.join(path,'data/dbtickets','HinRueckfahrt.pdf')
+            self.tickets = parser.parse_tickets(path)
+
+        def test_there_are_two_tickets(self):
+            assert len(self.tickets) == 2
+
+
+        def test_there_are_the_same_number_of_stops_as_stop_times(self):
+            for t in self.tickets:
+                assert len(t.stops) == len(t.stop_times)
+
+        def test_stops(self):
+            assert self.tickets[0].stops == ['Mannheim Hbf','Stuttgart Hbf','Stuttgart Hbf (tief)','Schwaikheim']
+            assert self.tickets[1].stops == ['Schwaikheim','Stuttgart Hbf (tief)','Stuttgart Hbf','Mannheim Hbf']
+
+        def test_stop_times(self):
+            assert self.tickets[0].stop_times[0] == dt.datetime(year=2018,month=9,day=17,hour=18,minute=00)
+            assert self.tickets[0].stop_times[1] == dt.datetime(year=2018,month=9,day=17,hour=18,minute=46)
+            assert self.tickets[0].stop_times[2] == dt.datetime(year=2018,month=9,day=17,hour=19,minute=00)
+            assert self.tickets[0].stop_times[3] == dt.datetime(year=2018,month=9,day=17,hour=19,minute=21)
+            assert self.tickets[1].stop_times[0] == dt.datetime(year=2018,month=9,day=19,hour=20,minute=53)
+            assert self.tickets[1].stop_times[1] == dt.datetime(year=2018,month=9,day=19,hour=21,minute=15)
+            assert self.tickets[1].stop_times[2] == dt.datetime(year=2018,month=9,day=19,hour=21,minute=51)
+            assert self.tickets[1].stop_times[3] == dt.datetime(year=2018,month=9,day=19,hour=22,minute=29)
